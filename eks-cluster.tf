37fd41414faf3e73d433225fc67d8e3200c6583b
segun <segun@darey.io>
2022-03-07 14:46:09 +0100

    learning l8s

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "18.8.1"
+  version         = "17.24.0"
   cluster_name    = local.cluster_name
   cluster_version = "1.21"
   subnets         = module.vpc.private_subnets
@@ -25,7 +25,7 @@ module "eks" {
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
       asg_desired_capacity          = 1
-    }
+    },
   ]
 }
 
